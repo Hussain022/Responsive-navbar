66577562c72add14983d79814e363df08075cb69
Hussain022 <111119731+Hussain022@users.noreply.github.com>
2024-03-01 17:38:50 +0600

    Update app.js

--- app.js
+++ app.js
@@ -24,3 +24,18 @@ const burger = () => {
         l3.style.position = 'absolute';
     }
 }
+
+const dflt = () => {
+    let x = document.querySelector('.navbar ul');
+    let l1 = document.querySelector('.line1');
+    let l2 = document.querySelector('.line2');
+    let l3 = document.querySelector('.line3');
+        x.style.left = '-200px';
+        x.style.transition = '.4s ease-in';
+        n = 1
+        l1.style.transform = 'unset';
+        l1.style.position = 'unset';
+        l2.style.display = 'unset';
+        l3.style.transform = 'unset';
+        l3.style.position = 'unset';
+}
